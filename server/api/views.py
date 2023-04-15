d4d748e67460418c313994859a01ab890f023fec
fondermark <publicnox@gmail.com>
2023-04-15 07:14:12 +0500

    0.0.14

--- server/api/views.py
+++ server/api/views.py
@@ -20,6 +20,35 @@ def get_posts_list(request):
 
 
 def get_employee_info(request):
-    employee_id = request.GET.get('id')
-    sql_string = f'SELECT * FROM main_employees me LEFT JOIN main_posts mp on mp.id = me.post_id WHERE me.id={employee_id}'
+    if request.method == 'GET':
+        employee_id = request.GET.get('id')
+        sql_string = f'SELECT * FROM main_employees me ' \
+                     f'LEFT JOIN main_posts mp on mp.id = me.post_id ' \
+                     f'WHERE me.id={employee_id}'
+    return JsonResponse(request_to_sql(sql_string)[0], safe=False)
+
+
+def get_post_info(request):
+    post_id = request.GET.get('id')
+    sql_string = f'SELECT * FROM main.main_posts WHERE id={post_id}'
+    return JsonResponse(request_to_sql(sql_string)[0], safe=False)
+
+
+def add_new_employeer(request):
+    lastname = request.POST.get('lastname')
+    firstname = request.POST.get('firstname')
+    surname = request.POST.get('surname')
+    post_id = request.POST.get('post_id')
+    age = request.POST.get('age')
+    gender = request.POST.get('gender')
+    sql_string = f'INSERT INTO main.main_employees (lastname, firstname, surname, post_id, age, gender)' \
+                 f'VALUES ({lastname}, {firstname}, {surname}, {post_id}, {age}, {gender})'
+    return JsonResponse(request_to_sql(sql_string)[0], safe=False)
+
+
+def add_new_post(request):
+    post_name = request.POST.get('post_name')
+    cat_name = request.POST.get('cat_name')
+    sql_string = f'INSERT INTO main.main_posts (post, category)' \
+                 f'VALUES ({post_name}, {cat_name})'
     return JsonResponse(request_to_sql(sql_string)[0], safe=False)
