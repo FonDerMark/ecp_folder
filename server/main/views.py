35fcc183c90432ad26fc25c4a5cee4522b0b9fb8
Dmitry <68408042+FonDerMark@users.noreply.github.com>
2023-04-19 02:12:50 +0500

    0.1.3 com added main

--- server/main/views.py
+++ server/main/views.py
@@ -1,37 +1,64 @@
 from django.shortcuts import render
 
-'''
-ТЗ предусматривает обмен данными посредством JSON,
-view функции целенаправленно не передают
-данные непосредственно в рендер и не обрабатывают данные.
-Фактически эмулируется работа любого фронта, и взаимодействие
-на уровне api.
-'''
-
 
 def index(request):
+    """
+    Функция для главной страницы.
+    :param request: HttpRequest объект, который содержит информацию о запросе
+    :return: html страницу с главной страницей.
+    """
     return render(request, 'index.html')
 
 
 def employeers_list(request):
+    """
+    Функция для страницы со списком работников.
+    :param request: HttpRequest объект, который содержит информацию о запросе
+    :return: html страницу со списком работников.
+    """
     return render(request, 'list_employeers.html')
 
 
 def employeer_edit(request):
+    """
+    Функция для страницы редактирования работника.
+    :param request: HttpRequest объект, который содержит информацию о запросе
+    :return: html страницу с формой редактирования работника.
+    """
     return render(request, 'edit_employeer.html')
 
 
 def employeer_add(request):
+    """
+    Функция для страницы добавления нового работника.
+    :param request: HttpRequest объект, который содержит информацию о запросе
+    :return: html страницу с формой добавления нового работника.
+    """
     return render(request, 'add_new_employeer.html')
 
 
 def posts_list(request):
+    """
+    Функция для страницы со списком должностей.
+    :param request: HttpRequest объект, который содержит информацию о запросе
+    :return: html страницу со списком должностей.
+    """
     return render(request, 'list_posts.html')
 
 
 def post_edit(request):
+    """
+    Функция для страницы редактирования должности.
+    :param request: HttpRequest объект, который содержит информацию о запросе
+    :return: html страницу с формой редактирования должности.
+    """
     return render(request, 'edit_post.html')
 
 
 def post_add(request):
+    """
+    Функция для страницы добавления нового должности.
+    :param request: HttpRequest объект, который содержит информацию о запросе
+    :return: html страницу с формой добавления нового должности.
+    """
     return render(request, 'add_new_post.html')
